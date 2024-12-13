998be9ae48af59d2a7a2c14b9265770f7955d837
Fabricio Belam <36649120+fbelam@users.noreply.github.com>
2024-12-13 07:41:46 -0300

    Add files via upload

---
+++ Automacao.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time 
+
+pyautogui.PAUSE = 0.3
+
+# pegar posições do mouse e da tela 
+print(pyautogui.position())
+print(pyautogui.size())
+
+#funções do mouse
+time.sleep(5)
+
+pyautogui.moveTo(x=498, y=967, duration=1)
+pyautogui.click(x=498, y=967)
+pyautogui.scroll(-200) # numero negativo - scroll para baixo 
+
---
+++ pegar_posicao.py
@@ -0,0 +1,6 @@
+import pyautogui
+import time 
+
+# pegar posição do mouse e da tela 
+time.sleep(5)
+print(pyautogui.position())
